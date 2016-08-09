3d17d25cd41f29e39bd8cf4588a163b3e387dd41
Alan Fidelino <afidedev26@gmail.com>
2016-08-09 06:23:40 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "Welcome to The RoR Tutorial Sample App"
+      redirect_to @user
     else
       render 'new'
     end
